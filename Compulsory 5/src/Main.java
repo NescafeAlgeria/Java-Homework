21f39d0e91df4e2544deb0b41eed87f3b74836d7
Mitila Alin <alinmitila03@gmail.com>
2025-03-22 12:41:30 +0200

    Added Compulsory 5

---
+++ Compulsory 5/src/Main.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        ImageRepository repository = new ImageRepository();
+
+        try {
+            // Adding sample images
+            Image img1 = new Image("A", LocalDate.now(), "C:/Images/A.jpg");
+            Image img2 = new Image("B", LocalDate.now(), "C:/Images/B.jpg");
+
+            repository.addImage(img1);
+            repository.addImage(img2);
+
+            // Listing images
+            repository.listImages();
+
+            // Displaying an image
+            repository.displayImage(img1);
+        } catch (ImageException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
